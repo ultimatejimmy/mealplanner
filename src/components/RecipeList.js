7db0e0961157ec6674685dc61849f66791f720f1
Jimmy Pautz <jpautz@dig.ms>
2019-01-14 15:29:03 -0600

    Fix issued due to weak typing by casting nest arrays and objects

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import Recipe from './Recipe'
+import React from "react";
+import Recipe from "./Recipe";
 
 class RecipeList extends React.Component {
-	render() { 
+	render() {
 		const weeks = this.props.weeks;
-		console.log(weeks);
-		const currentWeek = weeks.find( week => week.name === this.props.currentWeek);
-		const showRecipes = currentWeek.recipes.map(recipe => {
-			console.log(recipe.directions)
+		// console.log(weeks);
+		let currentWeek = weeks.find(
+			week => week.name == this.props.currentWeek
+		);
+		currentWeek = { ...currentWeek };
+		let currentRecipes = [currentWeek.recipes];
+		// currentRecipes = [...currentRecipes];
+		// console.log(currentRecipes);
+		const showRecipes = currentRecipes.map((recipe, index) => {
+			recipe = { ...recipe };
 			return (
-				<Recipe 
-					name={recipe.name} 
+				<Recipe
+					key={recipe.index}
+					name={recipe.name}
 					directions={recipe.directions}
 					ingredients={recipe.ingredients}
 				/>
-			)
-		})
+			);
+		});
 
-		
-		return ( 
+		return (
 			<div id="recipeList">
 				<h1>{this.props.currentWeek}</h1>
 				{showRecipes}
 			</div>
-		 );
+		);
 	}
 }
- 
+
 export default RecipeList;

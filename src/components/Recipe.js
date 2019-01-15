ea845887cead7d7aafa04d5ae2a1c656ae169836
Jimmy Pautz <jpautz@dig.ms>
2019-01-15 15:03:20 -0600

    Add new field to recipe page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,40 +1,46 @@
-import React from 'react'
-import InlineEdit from 'react-edit-inplace';
+import React from "react";
+import InlineEdit from "react-edit-inplace";
 
 export default class Recipe extends React.Component {
 	constructor(props) {
 		super();
 		this.state = {
-			name: 'Add recipe title',
-			directions: "asdfsdfsdfasdf asfasdf asdf "
-		}
+			name: "Add recipe title",
+			directions: "Add recipe directions here.",
+			source: "Add recipe source URL"
+		};
 		this.dataChanged = this.dataChanged.bind(this);
 	}
 	dataChanged(data) {
-        // data = { description: "New validated text comes here" }
-        // Update your model from here
-        console.log(data)
-        this.setState({...data})
+		// data = { description: "New validated text comes here" }
+		// Update your model from here
+		console.log(data);
+		this.setState({ ...data });
 	}
 	render() {
-		
-    	return (
-	 		<div className="recipe">
-			<InlineEdit
-				text={this.state.name}
-				paramName="name"
-				change={this.dataChanged}
-				staticElement="h2"
-            />
-			<InlineEdit
-				text={this.state.directions}
-				paramName="directions"
-				change={this.dataChanged}
-				staticElement="p"
-            />
-	   		<h3>Recipe Source</h3>
-			<b>Ingredients</b>
-		</div>
-    )
-  }
+		return (
+			<div className="recipe">
+				<InlineEdit
+					text={this.state.name}
+					paramName="name"
+					change={this.dataChanged}
+					staticElement="h2"
+				/>
+				<InlineEdit
+					text={this.state.directions}
+					paramName="directions"
+					change={this.dataChanged}
+					staticElement="p"
+				/>
+				<h3>Recipe Source</h3>
+				<InlineEdit
+					text={this.state.source}
+					paramName="source"
+					change={this.dataChanged}
+					staticElement="p"
+				/>
+				<h3>Ingredients</h3>
+			</div>
+		);
+	}
 }
